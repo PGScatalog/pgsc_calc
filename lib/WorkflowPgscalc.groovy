9994433c9c8a1c166f0b5ae87e9a9482e5787d0f
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2021-10-15 10:29:28 +0100

    Validate inputs

--- lib/WorkflowPgscalc.groovy
+++ lib/WorkflowPgscalc.groovy
@@ -3,44 +3,22 @@
 //
 
 class WorkflowPgscalc {
-
     //
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        genomeExistsError(params, log)
+        // TODO: decide how to provide / validate reference gnomes
+        // genomeExistsError(params, log)
 
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+        if (!params.input) {
+            log.error "Genome input file not specified with e.g. '--input genome.vcf.gz' or via a detectable config file."
             System.exit(1)
         }
-    }
 
-    //
-    // Get workflow summary for MultiQC
-    //
-    public static String paramsSummaryMultiqc(workflow, summary) {
-        String summary_section = ''
-        for (group in summary.keySet()) {
-            def group_params = summary.get(group)  // This gets the parameters of that particular group
-            if (group_params) {
-                summary_section += "    <p style=\"font-size:110%\"><b>$group</b></p>\n"
-                summary_section += "    <dl class=\"dl-horizontal\">\n"
-                for (param in group_params.keySet()) {
-                    summary_section += "        <dt>$param</dt><dd><samp>${group_params.get(param) ?: '<span style=\"color:#999999;\">N/A</a>'}</samp></dd>\n"
-                }
-                summary_section += "    </dl>\n"
-            }
-        }
-
-        String yaml_file_text  = "id: '${workflow.manifest.name.replace('/','-')}-summary'\n"
-        yaml_file_text        += "description: ' - this information is collected when the pipeline is started.'\n"
-        yaml_file_text        += "section_name: '${workflow.manifest.name} Workflow Summary'\n"
-        yaml_file_text        += "section_href: 'https://github.com/${workflow.manifest.name}'\n"
-        yaml_file_text        += "plot_type: 'html'\n"
-        yaml_file_text        += "data: |\n"
-        yaml_file_text        += "${summary_section}"
-        return yaml_file_text
+        if (!params.format) {
+	    log.error "Genotype input format not specified with e.g. '--format vcf'"
+	    System.exit(1)
+	}
     }
 
     //
