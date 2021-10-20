9aff34989b2e4892cdf10c09585c6e7487f1e4fc
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2021-10-20 17:29:13 +0100

    make bfile and vcf input mutually exclusive

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
-# TODO nf-core: Update the script to check the samplesheet
-# This script is based on the example at: https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
-
 import os
 import sys
 import errno
 import argparse
-
 
 def parse_args(args=None):
     Description = "Reformat nf-core/pgscalc samplesheet file and check its contents."
@@ -38,27 +34,33 @@ def print_error(error, context="Line", context_str=""):
     sys.exit(1)
 
 
-# TODO nf-core: Update the check_samplesheet function
 def check_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
 
-    sample,fastq_1,fastq_2
-    SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz
-    SAMPLE_PE,SAMPLE_PE_RUN2_1.fastq.gz,SAMPLE_PE_RUN2_2.fastq.gz
-    SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,
+    sample,path,spec
+    dataset1,dataset1.bed,bed
+    dataset1,dataset1.bim,bim
 
+    OR:
+
+    sample,path,spec
+    chr1,chr1.vcf.gz,vcf
+    chr2,chr2.vcf.gz,vcf
+    
+    TODO: update this
     For an example see:
     https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
     """
 
     sample_mapping_dict = {}
+
     with open(file_in, "r") as fin:
 
         ## Check header
         MIN_COLS = 2
         # TODO nf-core: Update the column names for the input samplesheet
-        HEADER = ["sample", "fastq_1", "fastq_2"]
+        HEADER = ["sample", "vcf_path", "bed_path", "bim_path"]
         header = [x.strip('"') for x in fin.readline().strip().split(",")]
         if header[: len(HEADER)] != HEADER:
             print("ERROR: Please check samplesheet header -> {} != {}".format(",".join(header), ",".join(HEADER)))
@@ -84,32 +86,38 @@ def check_samplesheet(file_in, file_out):
                 )
 
             ## Check sample name entries
-            sample, fastq_1, fastq_2 = lspl[: len(HEADER)]
+            sample, vcf_path, bed_path, bim_path = lspl[: len(HEADER)]
             sample = sample.replace(" ", "_")
             if not sample:
                 print_error("Sample entry has not been specified!", "Line", line)
 
-            ## Check FastQ file extension
-            for fastq in [fastq_1, fastq_2]:
-                if fastq:
-                    if fastq.find(" ") != -1:
-                        print_error("FastQ file contains spaces!", "Line", line)
-                    if not fastq.endswith(".fastq.gz") and not fastq.endswith(".fq.gz"):
-                        print_error(
-                            "FastQ file does not have extension '.fastq.gz' or '.fq.gz'!",
-                            "Line",
-                            line,
-                        )
+            ## Check file extensions
+            for path in [vcf_path, bed_path, bim_path]:
+                if path:
+                    if path.find(" ") != -1:
+                        print_error("File path contains spaces!", "Line", line)
+            for path in bed_path:
+                if not bed_path.endswith(".bed"):
+                    print_error(".bed file does not have extension .bed",
+                                "Line", line)
+            for path in bim_path:
+                if not bim_path.endswith(".bim"):
+                    print_error("bim file does not have extension .bim",
+                                "Line", line)
+            for path in vcf_path:
+                if not vcf_path.endswith(".vcf.gz"):
+                    print_error("VCF file does not have extension .vcf.gz",
+                                "Line", line)
 
-            ## Auto-detect paired-end/single-end
-            sample_info = []  ## [single_end, fastq_1, fastq_2]
-            if sample and fastq_1 and fastq_2:  ## Paired-end short reads
-                sample_info = ["0", fastq_1, fastq_2]
-            elif sample and fastq_1 and not fastq_2:  ## Single-end short reads
-                sample_info = ["1", fastq_1, fastq_2]
+            ## Auto-detect vcf or bfile
+            sample_info = [] # is_vcf, vcf, bed, bim
+            if sample and bed_path and bim_path:  ## bfiles
+                sample_info = ["0", vcf_path, bed_path, bim_path]
+            elif sample and vcf_path:  ## vcf
+                sample_info = ["1", vcf_path, bed_path, bim_path]
             else:
-                print_error("Invalid combination of columns provided!", "Line", line)
-
+                print_error("Invalid combination of columns provided! Have you got vcf and bed/bim for a sample?", "Line", line)
+                        
             ## Create sample mapping dictionary = { sample: [ single_end, fastq_1, fastq_2 ] }
             if sample not in sample_mapping_dict:
                 sample_mapping_dict[sample] = [sample_info]
@@ -124,15 +132,20 @@ def check_samplesheet(file_in, file_out):
         out_dir = os.path.dirname(file_out)
         make_dir(out_dir)
         with open(file_out, "w") as fout:
-            fout.write(",".join(["sample", "single_end", "fastq_1", "fastq_2"]) + "\n")
+            fout.write(",".join(["sample", "is_vcf", "vcf_path", "bed_path", "bim_path"]) + "\n")
+
+            is_vcf_set = set()
             for sample in sorted(sample_mapping_dict.keys()):
+                [is_vcf_set.add(x[0]) for x in sample_mapping_dict[sample]]
+            if not (len(is_vcf_set) == 1):
+                print_error("All samples must be in the same format! (e.g. all VCF or all bed / bim)")
 
                 ## Check that multiple runs of the same sample are of the same datatype
                 if not all(x[0] == sample_mapping_dict[sample][0][0] for x in sample_mapping_dict[sample]):
                     print_error("Multiple runs of a sample must be of the same datatype!", "Sample: {}".format(sample))
 
                 for idx, val in enumerate(sample_mapping_dict[sample]):
-                    fout.write(",".join(["{}_T{}".format(sample, idx + 1)] + val) + "\n")
+                    fout.write(",".join(sample) + "," + ",".join(val) + "\n")
     else:
         print_error("No entries to process!", "Samplesheet: {}".format(file_in))
 
@@ -140,7 +153,6 @@ def check_samplesheet(file_in, file_out):
 def main(args=None):
     args = parse_args(args)
     check_samplesheet(args.FILE_IN, args.FILE_OUT)
-
 
 if __name__ == "__main__":
     sys.exit(main())
