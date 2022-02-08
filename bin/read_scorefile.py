9c9f2706779cc7f5d221b7a9522f5e0886cac0c8
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-02-08 17:20:14 +0000

    return a dict of dataframes

--- bin/read_scorefile.py
+++ bin/read_scorefile.py
@@ -4,15 +4,17 @@ import numpy as np
 import pandas as pd
 import argparse
 import os.path
+import sys
 import pickle
 
-parser = argparse.ArgumentParser(description='Read and format scoring files')
-parser.add_argument('-s','--scorefiles', dest = 'scorefiles', nargs='+',
-                    help='<Required> Scorefile path (wildcard * is OK)', required=True)
-parser.add_argument('-o', '--outfile', dest = 'outfile', required = False,
-                    default = 'scorefiles.pkl',
-                    help = '<Required> Output path to pickled list of scorefiles, e.g. scorefiles.pkl')
-args = parser.parse_args()
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(description='Read and format scoring files')
+    parser.add_argument('-s','--scorefiles', dest = 'scorefiles', nargs='+',
+                        help='<Required> Scorefile path (wildcard * is OK)', required=True)
+    parser.add_argument('-o', '--outfile', dest = 'outfile', required = False,
+                        default = 'scorefiles.pkl',
+                        help = '<Required> Output path to pickled list of scorefiles, e.g. scorefiles.pkl')
+    return parser.parse_args()
 
 def to_int(i):
     # convert non-numeric chromosomes or positions to NaN
@@ -42,15 +44,13 @@ def set_effect_type(x):
         )
     return scorefile
 
-def quality_control(df, accession):
+def quality_control(df):
     return (
         df.query('effect_allele != "P" | effect_allele != "N"')
         .dropna(subset = ['chr_name', 'chr_position'])
-        .rename(columns={'effect_weight': 'effect_weight_{}'.format(accession)})
     )
 
 def read_scorefile(path):
-    accession = os.path.basename(path).split('.')[0]
     x = pd.read_table(path, converters = { "chr_name": to_int, "chr_pos": to_int
                                            }, comment = "#")
 
@@ -60,14 +60,18 @@ def read_scorefile(path):
     x[["chr_name", "chr_position"]] = x[["chr_name", "chr_position"]].astype(pd.Int64Dtype())
 
     scorefile = set_effect_type(x) # e.g. additive, recessive
-    qc_scorefile = quality_control(scorefile, accession)
+    qc_scorefile = quality_control(scorefile)
 
     return qc_scorefile
 
-def process_scorefiles(args):
+def main(args = None):
+    args = parse_args(args)
+    accessions = [os.path.basename(x).split('.')[0] for x in args.scorefiles]
     scorefiles = [read_scorefile(x) for x in args.scorefiles]
+    score_dict = dict(zip(accessions, scorefiles)) # key: accession, value: df
+
     with open(args.outfile, 'wb') as f:
-        pickle.dump(scorefiles, f)
+        pickle.dump(score_dict, f)
 
 if __name__ == "__main__":
-    process_scorefiles(args)
+    sys.exit(main())
