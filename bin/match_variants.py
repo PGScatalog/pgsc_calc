d25c1106bc10deaad026329ea21fdea7bc91652e
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-02-11 16:22:58 +0000

    add docstrings

--- bin/match_variants.py
+++ bin/match_variants.py
@@ -20,29 +20,66 @@ def parse_args(args=None):
     return parser.parse_args()
 
 def read_scorefiles(pkl):
+    ''' Read a pickled dict of dataframes (key: accession, value: scores df) '''
     jar = open(pkl, 'rb')
     scorefiles = pickle.load(jar)
     return scorefiles
 
 def match_ea_ref(scorefile, target):
-    # EA = REF and OA = ALT
+    """ Match effect allele in scorefile against reference allele in target.
+
+    Parameters:
+    scorefile (dataframe): A dataframe containing columns chr_name,
+        chr_position, effect_allele, other_allele, and effect weight.
+    target (dataframe): Containing columns #CHROM, POS, REF, ALT (the same
+        columns as a plink variant information file, e.g. bim or pvar)
+
+    Returns:
+        A dataframe with rows intersecting position and alleles, and columns merged
+    """
     return pd.merge(scorefile, target, how = 'inner', left_on = ['chr_name', \
         'chr_position', 'effect_allele', 'other_allele'], right_on = ['#CHROM', \
         'POS', 'REF', 'ALT'], validate = '1:1', suffixes=(False, False))
 
 def match_ea_alt(scorefile, target):
+    """ Match effect allele in scorefile against alternate allele in target.
+
+    Parameters:
+    scorefile (dataframe): A dataframe containing columns chr_name,
+        chr_position, effect_allele, other_allele, and effect weight.
+    target (dataframe): Containing columns #CHROM, POS, REF, ALT (the same
+        columns as a plink variant information file, e.g. bim or pvar)
+
+    Returns:
+        A dataframe with rows intersecting position and alleles, and columns merged
+    """
     # EA = ALT and OA = REF
     return pd.merge(scorefile, target, how = 'inner', left_on = ['chr_name', \
         'chr_position', 'effect_allele', 'other_allele'], right_on = ['#CHROM', \
         'POS', 'ALT', 'REF'], validate = '1:1', suffixes=(False, False))
 
 def complement(s):
+    ''' Complement a string of nucleotides '''
     basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     letters = list(s)
     letters = [basecomplement[base] for base in letters]
     return ''.join(letters)
 
 def match_variants(scorefile, target, accession, con):
+    """ Match variants in a scorefile against target variant information
+
+    Matching can be done by:
+        - Simple direct matching of positions and alleles
+        - Take unmatched alleles and try matching after complementing
+    Logs of the matching process are stored in a persistent database
+
+    Parameters:
+    scorefile: A dataframe containing columns chr_name, chr_position,
+        effect_allele, other_allele, effect_weight, and effect_type
+    target: A dataframe containing columns #CHROM, POS, REF, and ALT
+    accession: A unique identifier associated with a scorefile, e.g. "PGS001229"
+    con: A sqlite database connection used to store matches and statistics
+    """
     standard_match = (
         match_ea_ref(scorefile, target).append(match_ea_alt(scorefile, target))
         .assign(match_type='standard', accession = accession)
@@ -61,10 +98,8 @@ def match_variants(scorefile, target, accession, con):
 
     matches = standard_match.append(flip_match)
 
-    # log matches
     matches.to_sql('matches', con, if_exists = 'append', index = False)
 
-    # log metadata
     (
         pd.DataFrame(data = { 'accession': [accession], 'n_target': [target.shape[0]], 'n_scorefile': [scorefile.shape[0]] })
         .to_sql('meta', con, if_exists = 'append', index = False)
@@ -76,48 +111,78 @@ def match_variants(scorefile, target, accession, con):
     )
 
 def match_flipped(unmatched, target):
-    # complement both alleles and add to the unmatched variants in the scorefile
+    """ Match complemented effect allele from scorefile against target genomic variants.
+
+    Parameters:
+    unmatched (dataframe): A dataframe containing unmatched variants, with
+        columns chr_name, chr_position, effect_allele, other_allele, and effect
+        weight.
+    target (dataframe): Containing columns #CHROM, POS, REF, ALT (the same
+        columns as a plink variant information file, e.g. bim or pvar)
+
+    Returns:
+        A dataframe with rows intersecting position and alleles, and columns merged
+    """
     complemented = [x.map(complement) for x in [unmatched['effect_allele'], unmatched['other_allele']]]
     complemented.append(unmatched.drop(['effect_allele', 'other_allele'], axis = 1))
     unmatched_flipped = pd.concat(complemented, axis = 1)
 
     return match_ea_ref(unmatched_flipped, target).append(match_ea_alt(unmatched_flipped, target))
 
-def read_scorefiles(pkl):
-    jar = open(pkl, 'rb')
-    scorefiles = pickle.load(jar)
-    return scorefiles
-
 def merge_scorefiles(x, y):
+    ''' Combine multiple scoring files by appending effect weight columns '''
     return x.merge(y, on = ['ID', 'effect_allele', 'effect_type'], how = 'outer')
 
 def split_effect_type(df):
-    # split df by effect type (additive, dominant, or recessive) into a dict of
-    # dfs
+    ''' Split scorefile variant rows into subsets by effect type '''
     grouped = df.groupby('effect_type')
     return { k: grouped.get_group(k).drop('effect_type', axis = 1 ) for k, v in
         grouped.groups.items() }
 
 def unduplicate_variants(df):
-    # when merging a lot of scoring files, sometimes a variant might be duplicated
-    # this can happen when the effect allele differs at the same position, e.g.:
-    #     - chr1: chr2:20003:A:C A 0.3 NA
-    #     - chr1: chr2:20003:A:C C NA 0.7
-    # where the last two columns represent different scores.  plink demands
-    # unique identifiers! so need to split, score, and sum later
+    """ Find variant matches that have duplicate identifiers
 
-    # .duplicated() marks first duplicate element as True
-    # cats, cats, dogs -> False, True, False
+    When merging a lot of scoring files, sometimes a variant might be duplicated
+    this can happen when the effect allele differs at the same position, e.g.:
+        - chr1: chr2:20003:A:C A 0.3 NA
+        - chr1: chr2:20003:A:C C NA 0.7
+    where the last two columns represent different scores.  plink demands
+    unique identifiers! so need to split, score, and sum later
+
+    Parameters:
+    df: A dataframe containing all matches, with columns ID, effect_allele, and
+        effect_weight
+
+    Returns:
+    A dict with two keys:
+        - 'ea_ref': A dataframe containing unique variant identifiers
+        - 'ea_alt': A dataframe containing the duplicated variant identifiers
+    """
     ea_ref = ~df.duplicated(subset=['ID'], keep='first')
     ea_alt = ~ea_ref
-    # ~ negates for getting a subset of rows with a boolean series
     return { 'ea_ref': df[ea_ref], 'ea_alt': df[ea_alt] }
 
 def write_scorefiles(effect_type, scorefile, split):
-    '''
-    Write merged scorefiles to plink2 scorefile format, split by effect type,
-    duplicate variant identifiers, and optionally chromosome
-    '''
+    """ Write a merged scorefile to valid plink2 scorefile format
+
+    Multiple scores stored in separate scorefiles can be merged (merging
+    columns) to process many scores in parallel. A valid scorefile must contain
+    variants with the same effect type and cannot contain duplicate variant
+    identifiers. Optionally, output scorefiles should reflect the input target
+    data's split state. Large target datasets may be split to process
+    chromosomes in parallel. If unsplit scorefiles are used on split target
+    genomes, then warnings are produced by plink2. Let's fix it and be polite.
+
+    Parameters:
+    effect_type: The effect type associated with the scorefile (additive,
+        dominant, or recessive)
+    scorefile: The merged scorefile, maybe containing multiple scores in
+        additional columns
+    split: Should output files be split by chromosome?
+
+    Returns:
+    None, a set of scorefiles are written to disk.
+    """
 
     fout = '{et}_{dup}.scorefile'
 
@@ -149,6 +214,16 @@ def split_scorefile(df):
              chrom, split_df in df.groupby('chr')}
 
 def make_report(conn, min_overlap):
+    """ Make a table of summary statistics about the matching process
+
+    Parameters:
+    conn: A connection to the match sqlite database
+    min_overlap (float): The minimum proportion of variants that should match
+        from the scoring file to the target genomic data
+
+    Returns:
+    None, the table is written to report.csv
+    """
     report = pd.read_sql('select * from matches', conn)
     log = pd.read_sql('select * from meta', conn)
 
@@ -173,7 +248,10 @@ def make_report(conn, min_overlap):
         assert row['pass_filter'], match_error
 
 def main(args = None):
+    ''' Match variants from scorefiles against target variant information '''
+
     args = parse_args(args)
+
     # read inputs and set up database for logging-------------------------------
     target = pd.read_csv(args.target, sep = '\t')
     unpickled_scorefiles = read_scorefiles(args.scorefiles) # { accession: df }
@@ -195,6 +273,3 @@ def main(args = None):
 
 if __name__ == '__main__':
     sys.exit(main())
-
-# TO DO ========================================================================
-# TO DO: double check ambiguous alleles??
