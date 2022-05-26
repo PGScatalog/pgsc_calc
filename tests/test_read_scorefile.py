5ff648a8d1f7b0adafbd888f221bb71f68828cd3
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-05-26 14:11:07 +0100

    Revert "improve test coverage"
    This reverts commit 7fca9abfde19dab7ef262d2882b5053bb076fb19.

--- tests/test_read_scorefile.py
+++ tests/test_read_scorefile.py
@@ -1,130 +1,48 @@
 import pytest
 import numpy as np
 import pandas as pd
-import requests as req
-import gzip
 from pyliftover import LiftOver
-from bin.read_scorefile import *
+from bin.read_scorefile import to_int
+from bin.read_scorefile import parse_lifted_chrom
+from bin.read_scorefile import convert_coordinates
 
 @pytest.fixture
 def valid_chrom():
-    ''' Autosomes (1 - 22) are valid, which will cause problems for non-human species '''
     return 1
 
 @pytest.fixture
 def invalid_chrom():
-    ''' Only integer chromosomes are supported '''
     return "random string"
 
 @pytest.fixture
 def annotated_chrom():
-    ''' LiftOver sometimes returns annotated chromosomes '''
     return "22_annotations_that_are_annoying"
 
 @pytest.fixture
-def accession():
-    ''' A placeholder PGS Catalog accession '''
-    return "PGS000802"
-
-@pytest.fixture
 def hg38_coords():
-    ''' A dataframe of random variants, pos in GRCh38 '''
+    # random variants from PGS000802, pos in GRCh38 (dbSNP)
     d = {'rsid' : ['rs11903757', 'rs6061231'], 'chr_name': [2, 20], 'chr_position': [191722478, 62381861] }
     return pd.DataFrame(d)
 
 @pytest.fixture
 def hg38_to_hg19_coords(hg38_coords):
-    ''' A dataframe containing known good coordinates in GRCh37, from dbSNP '''
+    # two additional columns, chr and pos now in GRCh37 (from dbSNP)
     d = {'lifted_chr': [2, 20], 'lifted_pos': [192587204, 60956917] }
     return hg38_coords.join(pd.DataFrame(d, dtype = 'Int64'), how = 'outer')
 
 @pytest.fixture
 def hg19_unique_coords():
-    ''' A dataframe of coordinates that are deleted in hg38 and won't map '''
+    # these coordinates won't map from hg19 -> hg38 (deleted)
     d = {'chr_name': [22, 22, 22], 'chr_position': [22561610, 23412058, 28016883]}
     return pd.DataFrame(d)
 
 @pytest.fixture
-def hg38():
-    ''' The only input the workflow should accept, but equivalent to hg38 '''
-    return 'GRCh38'
-
-@pytest.fixture
-def hg19():
-    ''' The only input the workflow should accept, but equivalent to hg19 '''
-    return 'GRCh37'
-
-@pytest.fixture
 def lo_tohg19():
-    ''' pyliftover object reponsible for converting coordinates hg38 -> hg19 '''
     return LiftOver('hg38', 'hg19')
 
 @pytest.fixture
 def lo_tohg38():
-    ''' pyliftover object reponsible for converting coordinates hg19 -> hg38 '''
     return LiftOver('hg19', 'hg38')
-
-@pytest.fixture
-def min_lift():
-    ''' Minimum proportion of variants to successfully remap coordinates '''
-    return 0.95
-
-@pytest.fixture
-def scoring_file_noheader():
-    ''' Fetch a scorefile without genome build data in the metadata header '''
-    scorefile = req.get('https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/PGS000802/ScoringFiles/PGS000802.txt.gz')
-    with open('PGS000802.txt', 'wb') as f:
-        f.write(gzip.decompress(scorefile.content))
-    yield 'PGS000802.txt'
-    os.remove('PGS000802.txt')
-
-@pytest.fixture
-def scoring_file_header():
-    ''' Fetch a scorefile with genome build data in the metadata header '''
-    scorefile = req.get('https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/PGS000777/ScoringFiles/PGS000777.txt.gz')
-    with open('PGS000777.txt', 'wb') as f:
-        f.write(gzip.decompress(scorefile.content))
-    yield 'PGS000777.txt'
-    os.remove('PGS000777.txt')
-
-@pytest.fixture
-def out_pickle():
-    ''' A path to a pickle file (main output) '''
-    yield 'out.pkl'
-    os.remove('out.pkl')
-
-@pytest.fixture
-def multi_et_scorefile_df(scoring_file_noheader):
-    ''' Scorefile dataframe with multiple effect types '''
-    return pd.read_table(scoring_file_noheader, comment = '#')
-
-@pytest.fixture
-def bad_multi_et_scorefile_df(multi_et_scorefile_df):
-    ''' Scorefile dataframe with bad (mixed) effect types '''
-    return multi_et_scorefile_df.assign(is_dominant = True)
-
-@pytest.fixture
-def good_score_df():
-    ''' Scorefile dataframe with no effect type '''
-    d = {'chr_name': [22], 'chr_position': [22561610], 'effect_allele': 'A', 'other_allele': 'G', 'effect_weight': 1}
-    return pd.DataFrame(d)
-
-@pytest.fixture
-def duplicate_score_df(good_score_df):
-    ''' Bad scorefile dataframe with duplicate variants '''
-    return pd.concat([good_score_df, good_score_df])
-
-@pytest.fixture
-def multi_score_df(good_score_df):
-    ''' A scorefile dataframe with multiple effect weight columns '''
-    return (good_score_df
-            .rename( columns = { 'effect_weight': 'effect_weight_one' } )
-            .assign( effect_weight_two = 1 ))
-
-@pytest.fixture
-def df_cols():
-    ''' Expected column names of a parsed scorefile '''
-    return { 'chr_name', 'chr_position', 'effect_allele', 'other_allele', 'effect_weight', 'effect_type' }
 
 def test_valid_chrom(valid_chrom):
     assert to_int(valid_chrom) == 1
@@ -137,8 +55,7 @@ def test_invalid_chrom(invalid_chrom):
 def test_annotated_chrom(annotated_chrom):
     assert parse_lifted_chrom(annotated_chrom) == 22
 
-def test_remap(hg38_coords, lo_tohg19, lo_tohg38, hg38_to_hg19_coords, hg19_unique_coords):
-    ''' Test known genomic coordinates are valid when remapped '''
+def test_liftover(hg38_coords, lo_tohg19, lo_tohg38, hg38_to_hg19_coords, hg19_unique_coords):
     # test valid positions are mapped from (GRCh38 -> GRCh37)
     hg38_coords[['lifted_chr', 'lifted_pos']] = hg38_coords.apply(lambda x: convert_coordinates(x, lo_tohg19), axis = 1)
     assert hg38_coords.equals(hg38_to_hg19_coords)
@@ -149,90 +66,3 @@ def test_remap(hg38_coords, lo_tohg19, lo_tohg38, hg38_to_hg19_coords, hg19_uniq
         .isnull()
         .all(axis=None)
             )
-
-def test_liftover(accession, hg38_coords, hg38_to_hg19_coords, hg19, hg38, min_lift, hg19_unique_coords):
-    ''' Test liftover function, which is applied to each scoring file '''
-
-    lifted = liftover(accession, hg38_coords, hg38, hg19, min_lift)
-    assert lifted.equals(hg38_to_hg19_coords)
-
-    lift_same_build = liftover(accession, hg38_coords, hg38, hg38, min_lift)
-    assert lift_same_build.equals(hg38_coords)
-
-    # test min_overlap throws an error
-    with pytest.raises(AssertionError):
-        liftover(accession, hg19_unique_coords, hg19, hg38, min_lift)
-
-def test_read_build(accession, scoring_file_noheader, scoring_file_header):
-    ''' Test reading genome build from scoring file header metadata '''
-
-    assert read_build(scoring_file_noheader) is None
-    assert read_build(scoring_file_header) == 'hg19'
-
-    # now check the error checking
-    # lifting over without a build number should raise assertion error
-    with pytest.raises(AssertionError):
-        check_build(accession, read_build(scoring_file_noheader))
-    assert check_build(accession, read_build(scoring_file_header)) is None
-
-def test_read_scorefile(scoring_file_header, scoring_file_noheader, df_cols):
-    ''' Test reading a scorefile to a consistent dataframe layout '''
-    assert get_accession(scoring_file_header) == 'PGS000777'
-
-    # rsid without position information raises an error
-    with pytest.raises(AssertionError):
-        read_scorefile(scoring_file_header)
-
-    df_dict = read_scorefile(scoring_file_noheader)
-    assert 'PGS000802' in df_dict
-    # https://www.pgscatalog.org/score/PGS000802/
-    # 19 variants, 6 columns
-    assert df_dict['PGS000802'].shape == (19, 6)
-    assert set(df_dict['PGS000802'].columns) == df_cols
-
-def test_qc(accession, good_score_df, duplicate_score_df):
-    ''' Test simple quality control checks on a scorefile dataframe '''
-    with pytest.raises(AssertionError):
-        quality_control(accession, duplicate_score_df)
-
-    assert quality_control(accession, good_score_df).shape == (1, 5)
-
-def test_set_effect_type(accession, good_score_df, multi_et_scorefile_df, bad_multi_et_scorefile_df ):
-    ''' Check setting effect types for additive, recessive, and dominant '''
-
-    path = 'placeholder/path/' # only used to generate an error message
-
-    # the effect_column should contain the default effect type
-    df = set_effect_type(good_score_df, path)
-    assert df.effect_type.unique() == 'additive'
-
-    # the effect_type column should contain three possible types
-    df_multi_et = set_effect_type(multi_et_scorefile_df, path)
-    expected_ets = {'is_dominant', 'is_recessive', 'additive'}
-    assert set(df_multi_et.effect_type.unique()) == expected_ets
-
-    with pytest.raises(AssertionError):
-        # variants can never be dominant and recessive simultaneously
-        set_effect_type(bad_multi_et_scorefile_df, path)
-
-def test_multi_effect_weights(multi_score_df, df_cols):
-    ''' Check multiple effect weights are properly split into a dict of df '''
-    d = multi_ew(multi_score_df)
-    # check effect weights 'one' and 'two' are dict keys
-    assert set(d.keys()) == {'one', 'two'}
-
-    # ... and dict values are dfs with proper columns
-    assert set(d['one'].columns) == df_cols
-    assert set(d['two'].columns) == df_cols
-
-def test_main(scoring_file_noheader, scoring_file_header, out_pickle):
-    ''' Test reading a scorefile and writing to a pickled dict '''
-
-    main(args = ['-s', scoring_file_noheader, '-o', out_pickle])
-
-    with open(out_pickle, 'rb') as f:
-        result = pickle.load(f)
-    assert isinstance(result, dict)
-
-    with pytest.raises(SystemExit):
-        main(args = [''])
