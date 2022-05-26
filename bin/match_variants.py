6c9913193332eb78b712afad05bf62cc695fbf9b
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-05-26 14:11:07 +0100

    Revert "fix report with new log format"
    This reverts commit bacb4564b6a857787ff138af13be89964478bdb8.

--- bin/match_variants.py
+++ bin/match_variants.py
@@ -183,14 +183,10 @@ def read_log(conn):
         pl.col("effect_type").cast(pl.Categorical)
          ])
 
-def update_log(logs, matches, conn, min_overlap, dataset):
+def update_log(logs, matches, conn, min_overlap):
     match_clean = matches.drop(['REF', 'ALT', 'REF_FLIP', 'ALT_FLIP'])
     match_log = (logs.join(match_clean, left_on = ['chr_name', 'chr_position', 'effect_allele', 'other_allele', 'accession', 'effect_type', 'effect_weight'], right_on = ['chr_name', 'chr_position', 'effect_allele', 'other_allele', 'accession', 'effect_type', 'effect_weight'], how = 'left')
-               .with_columns([
-                   pl.col('ambiguous').fill_null(True),
-                   pl.lit(dataset).alias('dataset')
-               ]))
-
+               .with_column(pl.col('ambiguous').fill_null(True)))
 
     check_match(match_log, min_overlap)
     match_log.write_csv('log.csv')
@@ -228,7 +224,7 @@ def main(args = None):
     # update logs --------------------------------------------------------------
     conn = connect_db(args.db)
     logs = read_log(conn)
-    update_log(logs, matches, conn, args.min_overlap, args.dataset)
+    update_log(logs, matches, conn, args.min_overlap)
 
     # prepare for writing out --------------------------------------------------
     # write one combined scorefile for efficiency, but need extra file for each:
