24dedcd9993e76cf6cc0ea0637e4a4c59529b4b1
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-07-29 18:15:35 +0100

    add correlation test

---
+++ tests/scores/run_nxf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+bash ./tests/scores/setup.sh
+
+scoredir=$(cat tests/scores/score_dir.txt)
+scores="${scoredir}/*.txt.gz"
+
+nextflow run main.nf \
+    -ansi-log false \
+    --input ./tests/scores/samplesheet_annot.csv \
+    -c ./tests/config/score.config \
+    --max_cpus 1 \
+    --max_memory 16.GB \
+    --target_build GRCh37 \
+    --scorefile "$scores"
