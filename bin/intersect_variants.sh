5c4637c1391c659fdf1422d7eb581032b152f3d1
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-01-13 11:38:33 +0000

    remove uuoc + fix missing print for bims

--- bin/intersect_variants.sh
+++ bin/intersect_variants.sh
@@ -18,23 +18,20 @@ target_format=$3
 
 # reference is always pvar
 echo -e "CHR:POS:A0:A1\tID_REF\tREF_REF\tIS_INDEL" > ref_variants.txt
-cat $i_reference | \
     awk '!/^#/ {split($5, ALT, ",");
           for (a in ALT){
             if($4 < ALT[a]) print $1":"$2":"$4":"ALT[a], $3, $4, (length($4) > 1 || length(ALT[a]) > 1); else print $1":"$2":"ALT[a]":"$4, $3, $4, (length($4) > 1 || length(ALT[a]) > 1)
-          }}' | sort >> ref_variants.txt
+          }}' $i_reference | sort >> ref_variants.txt
 
 # handle target (in multiple formats)
 echo -e "CHR:POS:A0:A1\tID_TARGET\tREF_TARGET" > target_variants.txt
 if [ "$target_format" == "pvar" ]; then
-    cat $i_target |\
     awk '!/^#/ {split($5, ALT, ",");
           for (a in ALT){
             if($4 < ALT[a]) print $1":"$2":"$4":"ALT[a], $3, $4; else print $1":"$2":"ALT[a]":"$4, $3, $4
-          }}' | sort >> target_variants.txt
+          }}' $i_target | sort >> target_variants.txt
 elif [ "$target_format" == "bim" ]; then
-  cat $i_target |\
-    awk '!/^#/ {if($5 < $6) print $1":"$4":"$5":"$6, $2, $6; else $1":"$4":"$6":"$5, $2, $6}' | sort >> target_variants.txt
+    awk '!/^#/ { if($5 < $6) print $1":"$4":"$5":"$6, $2, $6; else print $1":"$4":"$6":"$5, $2, $6}' $i_target | sort >> target_variants.txt
 else
   echo "${target_format} is not a valid option (only pvar and bim are currently accepted)"
   exit 1
