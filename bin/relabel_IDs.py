bbc13bf21349e991ac5ed5e24677a9462e7780c3
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-02-02 16:21:43 +0000

    add support for a list of mapping files

--- bin/relabel_IDs.py
+++ bin/relabel_IDs.py
@@ -1,34 +1,44 @@
+#!/usr/bin/env python3
+
 import argparse
+from functools import reduce
+import operator
 
 
 def _parse_args(args=None):
-    parser = argparse.ArgumentParser(description="Relabel the column values in one file based on a pair of columns in another",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(help='mapping filename', dest='map_file')
-    parser.add_argument(help='column to change FROM', dest='col_from')
-    parser.add_argument(help='column to change TO', dest='col_to')
+    parser = argparse.ArgumentParser(
+        description="Relabel the column values in one file based on a pair of columns in another",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-m", "--maps", help='mapping filenames', dest='map_files', nargs='+')
+    parser.add_argument("--col_from", help='column to change FROM', dest='col_from')
+    parser.add_argument("--col_to", help='column to change TO', dest='col_to')
+    parser.add_argument("--target_file", help='target file', dest='target_file')
+    parser.add_argument("--target_col", help='target column to revalue', dest='target_col')
 
-    parser.add_argument(help='target file', dest='target_file')
-    parser.add_argument(help='target column to revalue', dest='target_col')
-
-    parser.add_argument(help='output filename', dest='out_file')
+    parser.add_argument("--out", help='output filename', dest='out_file')
 
     return parser.parse_args(args)
 
 
-def relabel_IDs():
-    args = _parse_args()
-
+def read_map(path, col_from, col_to):
     # Read the mapping file
-    with open(args.map_file, 'r') as in_map:
+    with open(path, 'r') as in_map:
         h = in_map.readline().strip().split()
-        i_from = h.index(args.col_from)
-        i_to = h.index(args.col_to)
+        i_from = h.index(col_from)
+        i_to = h.index(col_to)
 
         mapping = {}
         for line in in_map:
             line = line.strip().split()
             mapping[line[i_from]] = line[i_to]
+    return mapping
+
+
+def relabel_IDs():
+    args = _parse_args()
+
+    map_list = [read_map(x, args.col_from, args.col_to) for x in args.map_files]
+    mapping = reduce(operator.ior, map_list, {})  # merge dicts quickly, ior is equivalent to | operator
 
     # Read, relabel and output file
     with open(args.out_file, 'w') as outf:
