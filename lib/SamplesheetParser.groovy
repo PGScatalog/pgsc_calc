64444e1695036ba85571a6adf541fd5562b85551
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-02-14 16:13:46 +0000

    implement groovy functions for samplesheet parsing

--- lib/SamplesheetParser.groovy
+++ lib/SamplesheetParser.groovy
@@ -1,107 +1,21 @@
-//
-// Check input samplesheet and get read channels
-//
-
-include { COMBINE_SCOREFILES  } from '../../modules/local/combine_scorefiles'
-
-//sp = new SamplesheetParser()
-//println sp.hi()
-
-workflow INPUT_CHECK {
-    take:
-    input_path // file: /path/to/samplesheet.csv
-    format // csv or JSON
-    scorefile // flat list of paths
-    chain_files
-
-    main:
-    /* all genomic data should be represented as a list of : [[meta], file]
-
-       meta hashmap structure:
-        id: experiment label, possibly shared across split genomic files
-        is_vcf: boolean, is in variant call format
-        is_bfile: boolean, is in PLINK1 fileset format
-        is_pfile: boolean, is in PLINK2 fileset format
-        chrom: The chromosome associated with the file. If multiple chroms, null.
-        n_chrom: Total separate chromosome files per experiment ID
-     */
-
-    ch_versions = Channel.empty()
-    parsed_input = Channel.empty()
-    
-    input = Channel.fromPath(input_path, checkIfExists: true)
-
-    if (format.equals("csv")) {
-        in_ch = input.splitCsv(header:true)       
-        verifySamplesheet(in_ch)
-        def n_chrom
-        n_chrom = file(input_path).countLines() - 1 // ignore header
-        in_ch
-            .map { row -> parseSamplesheet(row, input_path, n_chrom) }
-            .set { parsed_input }
-    } else if (format.equals("json")) {
-        in_ch = input.splitJson()
-        def n_chrom
-        n_chrom = file(input_path).countJson() // ignore header
-        throw new Exception("Not implemented")
-    }
-
-    parsed_input.branch {
-                vcf: it[0].is_vcf
-                bfile: it[0].is_bfile
-                pfile: it[0].is_pfile
-        }
-        .set { ch_branched }
-
-    // branch is like a switch statement, so only one bed / bim was being
-    // returned
-    ch_branched.bfile.multiMap { it ->
-        bed: [it[0], it[1][0]]
-        bim: [it[0], it[1][1]]
-        fam: [it[0], it[1][2]]
-    }
-        .set { ch_bfiles }
-
-    ch_branched.pfile.multiMap { it ->
-        pgen: [it[0], it[1][0]]
-        psam: [it[0], it[1][1]]
-        pvar: [it[0], it[1][2]]
-    }
-        .set { ch_pfiles }
-    
-    COMBINE_SCOREFILES ( scorefile, chain_files )
-
-    versions = ch_versions.mix(COMBINE_SCOREFILES.out.versions)
-
-    ch_bfiles.bed.mix(ch_pfiles.pgen).dump(tag: 'input').set { geno }
-    ch_bfiles.bim.mix(ch_pfiles.pvar).dump(tag: 'input').set { variants }
-    ch_bfiles.fam.mix(ch_pfiles.psam).dump(tag: 'input').set { pheno }
-    ch_branched.vcf.dump(tag: 'input').set{vcf}
-    COMBINE_SCOREFILES.out.scorefiles.dump(tag: 'input').set{ scorefiles }
-    COMBINE_SCOREFILES.out.log_scorefiles.dump(tag: 'input').set{ log_scorefiles }
-
-    emit:
-    geno = Channel.empty()
-    variants = Channel.empty()
-    pheno = Channel.empty()
-    vcf = Channel.empty()
-    scorefiles = Channel.empty()
-    log_scorefiles = Channel.empty()
-    versions = Channel.empty()
-}
-
 import java.nio.file.Paths
 import java.nio.file.NoSuchFileException
 
-def parseSamplesheet(row, samplesheet_path, n_chrom) {
-    // [[meta], [path, to targets]]
-    def chrom = truncateChrom(row)
-    def dosage = importDosage(row)
-    def paths = getFilePaths(row, samplesheet_path)
+class SamplesheetParser {
+    def hi() {
+        return "Hello, workshop participants!"
+    }
+
+    def parseSamplesheet(row, samplesheet_path, n_chrom) {
+        // [[meta], [path, to targets]]
+        def chrom = truncateChrom(row)
+        def dosage = importDosage(row)
+        def paths = getFilePaths(row, samplesheet_path)
     
-    path_list = paths["path"]
-    path_map = paths.subMap("is_bfile", "is_pfile", "is_vcf", "format")
-    return [[sampleset: row.sampleset, chrom: chrom, vcf_import_dosage: dosage, n_chrom: n_chrom] + path_map, path_list]
+        path_list = paths["path"]
+        path_map = paths.subMap("is_bfile", "is_pfile", "is_vcf", "format")
+        return [[sampleset: row.sampleset, chrom: chrom, vcf_import_dosage: dosage, n_chrom: n_chrom] + path_map, path_list]
+    }
 }
 
 def importDosage(row) {
