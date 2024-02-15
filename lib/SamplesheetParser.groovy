131bfc5fdf052b6ee07fa6d8b678918e31aea7ad
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-02-15 11:29:39 +0000

    set up utility class

--- lib/SamplesheetParser.groovy
+++ lib/SamplesheetParser.groovy
@@ -1,146 +1,176 @@
-import java.nio.file.Paths
 import java.nio.file.NoSuchFileException
+import java.nio.file.Path
+import nextflow.Nextflow
 
 class SamplesheetParser {
-    def hi() {
-        return "Hello, workshop participants!"
+    Path path
+    Integer n_chrom
+
+    SamplesheetParser(path, n_chrom) {
+        this.path = path
+        this.n_chrom = n_chrom
     }
 
-    def parseSamplesheet(row, samplesheet_path, n_chrom) {
-        // [[meta], [path, to targets]]
-        def chrom = truncateChrom(row)
-        def dosage = importDosage(row)
-        def paths = getFilePaths(row, samplesheet_path)
-    
-        path_list = paths["path"]
-        path_map = paths.subMap("is_bfile", "is_pfile", "is_vcf", "format")
-        return [[sampleset: row.sampleset, chrom: chrom, vcf_import_dosage: dosage, n_chrom: n_chrom] + path_map, path_list]
-    }
-}
+    def parseCSVRow(row) {
+        def parsed_row = [:]
+        parsed_row.id = row.sampleset
+        parsed_row.n_chrom = this.n_chrom
+        parsed_row.chrom = truncateChrom(row)
+        parsed_row.format = row.format
+        parsed_row.vcf_import_dosage = importDosage(row)
+        parsed_row = parsed_row + getFlagMap(row)
 
-def importDosage(row) {
-    def vcf_import_dosage = false
-    if (row.containsKey("vcf_genotype_field")) {
-        if (row["vcf_genotype_field"] == "DS") {
-            vcf_import_dosage = true
+        return [parsed_row, getFilePaths(row)]
+    }
+
+    def verifySamplesheet(rows) {
+        checkChroms(rows)
+        checkOneSampleset(rows)
+        checkDuplicateChromosomes(rows)
+        checkReservedName(rows)
+        return rows
+    }
+
+    def getFlagMap(row) {
+        // make a map with some helpful bool flags. useful for both JSON and CSV
+        def flags = [:]
+        flags.is_vcf = false
+        flags.is_bfile = false
+        flags.is_pfile = false
+
+        switch (row.format) {
+            case "pfile":
+                flags.is_pfile = true
+                break
+            case "bfile":
+                flags.is_bfile = true
+                break
+            case "vcf":
+                flags.is_vcf = true
+                break
+            default:
+                Nextflow.error("Invalid format: ${row.format}")
+        }
+        return flags
+    }
+
+    private static def truncateChrom(row) {
+        // when plink recodes chromosomes, it drops chr prefix. make sure the samplesheet matches this
+        return row.chrom ? row.chrom.toString().replaceFirst("chr", "") : "ALL"
+    }
+
+    private def getFilePaths(row) {
+        // return a list in order of geno, variants, pheno
+        def resolved_path = resolvePath(row.path_prefix)
+        def suffix = [:]
+
+        switch (row.format) {
+            case "pfile":
+                suffix = [variants: ".pvar", geno: ".pgen", pheno: ".psam"]
+                break
+            case "bfile":
+                suffix = [variants: ".bim", geno: ".bed", pheno: ".fam"]
+                break
+            case "vcf":
+                // gzip compression gets picked up later
+                suffix = [variants: ".vcf", geno: ".vcf", pheno: ".vcf"]
+                break
+            default:
+                Nextflow.error("Invalid format: ${row.format}")
+        }
+
+        // automatically prefer compressed variant information data (and vcfs)
+        def variant_path = suffix.subMap("variants").collect { k, v ->
+            def f
+            try {
+                // always prefer zstd compressed data (nobody does this to VCFs... hopefully)
+                f = Nextflow.file(resolved_path + v + ".zst", checkIfExists: true)
+            }
+            catch (NoSuchFileException zst_e) {
+                try {
+                    // but gzipped is OK too
+                    f = Nextflow.file(resolved_path + v + ".gz", checkIfExists: true)
+                } catch (NoSuchFileException gzip_e) {
+                    // try uncompressed data as last resort
+                    f = Nextflow.file(resolved_path + v, checkIfExists: true)
+                }
+            }
+            return f
+        }
+
+        def other_paths = suffix.subMap(["geno", "pheno"]).collect { k, v ->
+            Nextflow.file(resolved_path + v, checkIfExists: true)
+        }
+
+        // call unique to remove duplicate entries for VCFs, adding at index to preserve order
+        def path_list = other_paths.plus(1, variant_path).unique()
+
+        return path_list
+    }
+
+    private def resolvePath(path) {
+        // paths in a CSV samplesheet might be relative, and should be resolved from the samplesheet path
+        def is_absolute = path.startsWith('/') // isAbsolute() was causing weird issues
+
+        def resolved_path
+        if (is_absolute) {
+            resolved_path = Nextflow.file(path).resolve()
+        } else {
+            resolved_path = Nextflow.file(this.path).getParent().resolve(path)
+        }
+
+        return resolved_path
+    }
+
+    private static def importDosage(row) {
+        // vcf_genotype_field is an optional field in the samplesheet
+        def vcf_import_dosage = false
+        if (row.containsKey("vcf_genotype_field")) {
+            if (row["vcf_genotype_field"] == "DS") {
+                vcf_import_dosage = true
+            }
+        }
+
+        return vcf_import_dosage
+    }
+
+    // samplesheet verification methods from here
+
+    private def checkChroms(rows) {
+        // one missing chromosome (i.e. a combined file) is OK. more than this isn't
+        def chroms = rows.collect { row -> row.chrom }
+        def n_empty_chrom = chroms.count { it == "" }
+        if (n_empty_chrom > 1) {
+            Nextflow.error("${n_empty_chrom} missing chromosomes detected! Maximum is 1. Check your samplesheet.")
         }
     }
 
-    return vcf_import_dosage
-}
-
-def getFilePaths(row, samplesheet_path) {
-    // return a list in order of geno, variants, pheno
-    def resolved_path = resolvePath(row.path_prefix, samplesheet_path)
-    def suffix = [:]
-    def is_vcf = false
-    def is_bfile = false
-    def is_pfile = false
-
-    switch(row.format) {
-        case "pfile":
-            suffix = [variants: ".pvar", geno: ".pgen", pheno: ".psam"]
-            is_pfile = true
-            break
-        case "bfile":
-            suffix = [variants: ".bim", geno: ".bed", pheno: ".fam"]
-            is_bfile = true
-            break
-        case "vcf":
-            suffix = [variants: ".vcf.gz", geno: ".vcf.gz", pheno: ".vcf.gz"]
-            is_vcf = true
-            break
-        default:
-            throw new Exception("Invalid format: ${format}")
-    }
-
-    variant_paths = suffix.subMap("variants").collect { k, v ->
-        try {
-            // always prefer zstd compressed data
-            f = file(resolved_path + v + ".zst", checkIfExists: true)
+    private def checkOneSampleset(rows) {
+        def samplesets = rows.collect { row -> row.sampleset }
+        def n_samplesets = samplesets.toSet().size()
+        if (n_samplesets > 1) {
+            Nextflow.error("${n_samplesets} missing chromosomes detected! Maximum is 1. Check your samplesheet.")
         }
-        catch (NoSuchFileException exception) {
-            f = file(resolved_path + v, checkIfExists: true)
+    }
+
+    private def checkReservedName(samplesheet) {
+        def samplesets = samplesheet.collect { row -> row.sampleset }
+        def n_bad_name = samplesets.count { it == "reference" }
+
+        if (n_bad_name != 0) {
+            Nextflow.error("Reserved sampleset name detected. Please don't call your sampleset 'reference'")
         }
-        return [(k): f]
-    }.first()
-
-    other_paths = suffix.subMap(["geno", "pheno"]).collect { k, v ->
-        [(k) : file(resolved_path + v, checkIfExists:true)]
     }
 
-    // flatten the list of maps
-    flat_path_map = other_paths.inject([:], { item, other -> item + other }) + variant_paths
+    private def checkDuplicateChromosomes(samplesheet) {
+        def chroms = samplesheet.collect { row -> row.chrom }
+        def n_unique_chroms = chroms.toSet().size()
+        def n_chroms = chroms.size()
 
-    // call unique to remove duplicate VCF entries
-    path_list = [flat_path_map.geno, flat_path_map.variants, flat_path_map.pheno].unique()
-    return [path: path_list, is_bfile: is_bfile, is_pfile: is_pfile, is_vcf: is_vcf, format: row.format]
-}
-
-def resolvePath(path, samplesheet_path) {
-    // isAbsolute() was causing weird issues
-    def is_absolute = path.startsWith('/')
-
-    def resolved_path
-    if (is_absolute) {
-        resolved_path = file(path).resolve()
-    } else {
-        resolved_path = file(samplesheet_path).getParent().resolve(path)
+        if (n_unique_chroms != n_chroms) {
+            Nextflow.error("Duplicated chromosome entries detected in samplesheet. Check your samplesheet.")
+        }
     }
 
-    return resolved_path
 }
 
-def truncateChrom(row) {
-    return row.chrom ? row.chrom.toString().replaceFirst("chr", "") : "ALL"
-}
-
-
-def verifySamplesheet(samplesheet) {
-    // input must be a file split with headers e.g. splitCsv or splitJSON
-    checkChroms(samplesheet)
-    checkOneSampleset(samplesheet)
-    checkReservedName(samplesheet)
-    checkDuplicateChromosomes(samplesheet)
-
-}
-
-def checkChroms(samplesheet) {
-    // one missing chromosome (i.e. a combined file) is OK. more than this isn't
-    samplesheet.collect{ row -> row.chrom }.map { it ->
-        n_empty_chrom = it.count { it == "" }
-          if (n_empty_chrom > 1) {
-            throw new Exception("${n_empty_chrom} missing chromosomes detected! Maximum is 1. Check your samplesheet.")    
-          }
-    }    
-}
-
-def checkOneSampleset(samplesheet) {
-    samplesheet.collect{ row -> row.sampleset }.map { it -> 
-        n_samplesets = it.toSet().size() 
-          if (n_samplesets > 1) {
-            throw new Exception("${n_samplesets} missing chromosomes detected! Maximum is 1. Check your samplesheet.")    
-          }
-    }    
-}
-
-def checkReservedName(samplesheet) {
-    samplesheet.collect{ row -> row.sampleset }.map { it -> 
-          n_bad_name = it.count { it == "reference" }    
-
-          if (n_bad_name > 0) {
-            throw new Exception("Reserved sampleset name detected. Please don't call your sampleset 'reference'")
-          }
-    }    
-}
-
-def checkDuplicateChromosomes(samplesheet) {
-    samplesheet.collect{ row -> row.chrom }.map { it ->
-          n_unique_chroms = it.toSet().size()
-          n_chroms = it.size()
-
-          if (n_unique_chroms != n_chroms) {
-             throw new Exception("Duplicated chromosome entries detected in samplesheet")
-          }
-    }    
-}
--- subworkflows/local/input_check.nf
+++ subworkflows/local/input_check.nf
@@ -4,8 +4,6 @@
 
 include { COMBINE_SCOREFILES  } from '../../modules/local/combine_scorefiles'
 
-//sp = new SamplesheetParser()
-//println sp.hi()
 
 workflow INPUT_CHECK {
     take:
@@ -32,13 +30,15 @@ workflow INPUT_CHECK {
     input = Channel.fromPath(input_path, checkIfExists: true)
 
     if (format.equals("csv")) {
-        in_ch = input.splitCsv(header:true)       
-        verifySamplesheet(in_ch)
         def n_chrom
         n_chrom = file(input_path).countLines() - 1 // ignore header
-        in_ch
-            .map { row -> parseSamplesheet(row, input_path, n_chrom) }
-            .set { parsed_input }
+        parser = new SamplesheetParser(file(input_path), n_chrom)   
+        input.splitCsv(header:true)
+                .collect()
+                .map { rows -> parser.verifySamplesheet(rows) }
+                .flatten()
+                .map { row -> parser.parseCSVRow(row)}
+                .set { parsed_input }
     } else if (format.equals("json")) {
         in_ch = input.splitJson()
         def n_chrom
@@ -81,152 +81,12 @@ workflow INPUT_CHECK {
     COMBINE_SCOREFILES.out.log_scorefiles.dump(tag: 'input').set{ log_scorefiles }
 
     emit:
-    geno = Channel.empty()
-    variants = Channel.empty()
-    pheno = Channel.empty()
-    vcf = Channel.empty()
-    scorefiles = Channel.empty()
-    log_scorefiles = Channel.empty()
-    versions = Channel.empty()
+    geno
+    variants
+    pheno
+    vcf
+    scorefiles
+    log_scorefiles
+    versions
 }
 
-import java.nio.file.Paths
-import java.nio.file.NoSuchFileException
-
-def parseSamplesheet(row, samplesheet_path, n_chrom) {
-    // [[meta], [path, to targets]]
-    def chrom = truncateChrom(row)
-    def dosage = importDosage(row)
-    def paths = getFilePaths(row, samplesheet_path)
-    
-    path_list = paths["path"]
-    path_map = paths.subMap("is_bfile", "is_pfile", "is_vcf", "format")
-    return [[sampleset: row.sampleset, chrom: chrom, vcf_import_dosage: dosage, n_chrom: n_chrom] + path_map, path_list]
-}
-
-def importDosage(row) {
-    def vcf_import_dosage = false
-    if (row.containsKey("vcf_genotype_field")) {
-        if (row["vcf_genotype_field"] == "DS") {
-            vcf_import_dosage = true
-        }
-    }
-
-    return vcf_import_dosage
-}
-
-def getFilePaths(row, samplesheet_path) {
-    // return a list in order of geno, variants, pheno
-    def resolved_path = resolvePath(row.path_prefix, samplesheet_path)
-    def suffix = [:]
-    def is_vcf = false
-    def is_bfile = false
-    def is_pfile = false
-
-    switch(row.format) {
-        case "pfile":
-            suffix = [variants: ".pvar", geno: ".pgen", pheno: ".psam"]
-            is_pfile = true
-            break
-        case "bfile":
-            suffix = [variants: ".bim", geno: ".bed", pheno: ".fam"]
-            is_bfile = true
-            break
-        case "vcf":
-            suffix = [variants: ".vcf.gz", geno: ".vcf.gz", pheno: ".vcf.gz"]
-            is_vcf = true
-            break
-        default:
-            throw new Exception("Invalid format: ${format}")
-    }
-
-    variant_paths = suffix.subMap("variants").collect { k, v ->
-        try {
-            // always prefer zstd compressed data
-            f = file(resolved_path + v + ".zst", checkIfExists: true)
-        }
-        catch (NoSuchFileException exception) {
-            f = file(resolved_path + v, checkIfExists: true)
-        }
-        return [(k): f]
-    }.first()
-
-    other_paths = suffix.subMap(["geno", "pheno"]).collect { k, v ->
-        [(k) : file(resolved_path + v, checkIfExists:true)]
-    }
-
-    // flatten the list of maps
-    flat_path_map = other_paths.inject([:], { item, other -> item + other }) + variant_paths
-
-    // call unique to remove duplicate VCF entries
-    path_list = [flat_path_map.geno, flat_path_map.variants, flat_path_map.pheno].unique()
-    return [path: path_list, is_bfile: is_bfile, is_pfile: is_pfile, is_vcf: is_vcf, format: row.format]
-}
-
-def resolvePath(path, samplesheet_path) {
-    // isAbsolute() was causing weird issues
-    def is_absolute = path.startsWith('/')
-
-    def resolved_path
-    if (is_absolute) {
-        resolved_path = file(path).resolve()
-    } else {
-        resolved_path = file(samplesheet_path).getParent().resolve(path)
-    }
-
-    return resolved_path
-}
-
-def truncateChrom(row) {
-    return row.chrom ? row.chrom.toString().replaceFirst("chr", "") : "ALL"
-}
-
-
-def verifySamplesheet(samplesheet) {
-    // input must be a file split with headers e.g. splitCsv or splitJSON
-    checkChroms(samplesheet)
-    checkOneSampleset(samplesheet)
-    checkReservedName(samplesheet)
-    checkDuplicateChromosomes(samplesheet)
-
-}
-
-def checkChroms(samplesheet) {
-    // one missing chromosome (i.e. a combined file) is OK. more than this isn't
-    samplesheet.collect{ row -> row.chrom }.map { it ->
-        n_empty_chrom = it.count { it == "" }
-          if (n_empty_chrom > 1) {
-            throw new Exception("${n_empty_chrom} missing chromosomes detected! Maximum is 1. Check your samplesheet.")    
-          }
-    }    
-}
-
-def checkOneSampleset(samplesheet) {
-    samplesheet.collect{ row -> row.sampleset }.map { it -> 
-        n_samplesets = it.toSet().size() 
-          if (n_samplesets > 1) {
-            throw new Exception("${n_samplesets} missing chromosomes detected! Maximum is 1. Check your samplesheet.")    
-          }
-    }    
-}
-
-def checkReservedName(samplesheet) {
-    samplesheet.collect{ row -> row.sampleset }.map { it -> 
-          n_bad_name = it.count { it == "reference" }    
-
-          if (n_bad_name > 0) {
-            throw new Exception("Reserved sampleset name detected. Please don't call your sampleset 'reference'")
-          }
-    }    
-}
-
-def checkDuplicateChromosomes(samplesheet) {
-    samplesheet.collect{ row -> row.chrom }.map { it ->
-          n_unique_chroms = it.toSet().size()
-          n_chroms = it.size()
-
-          if (n_unique_chroms != n_chroms) {
-             throw new Exception("Duplicated chromosome entries detected in samplesheet")
-          }
-    }    
-}
